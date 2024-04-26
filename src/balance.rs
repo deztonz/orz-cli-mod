7d17e6bf170cbcc7de3203e5f9f01e56eea12dad
Ton <nataponz@gmail.com>
2024-04-26 14:17:08 +0700

    Update balance.rs

--- src/balance.rs
+++ src/balance.rs
@@ -1,23 +1,16 @@
-use std::str::FromStr;
-
-use solana_program::pubkey::Pubkey;
-use solana_sdk::signature::Signer;
-
+use solana_sdk::{
+    signature::Signer,
+    signer::keypair::Keypair
+};
 use crate::Miner;
 
 impl Miner {
-    pub async fn balance(&self, address: Option<String>) {
-        let signer = self.signer();
-        let address = if let Some(address) = address {
-            if let Ok(address) = Pubkey::from_str(&address) {
-                address
-            } else {
-                println!("Invalid address: {:?}", address);
-                return;
-            }
-        } else {
-            signer.pubkey()
-        };
+    pub async fn balance_by_number(&self, keypair_number: u64) {
+        self.balance(&self.signer_by_number(keypair_number)).await
+    }
+
+    pub async fn balance(&self, signer: &Keypair) {
+        let address = signer.pubkey();
         let client = self.rpc_client.clone();
         let token_account_address = spl_associated_token_account::get_associated_token_address(
             &address,
@@ -26,7 +19,7 @@ impl Miner {
         match client.get_token_account(&token_account_address).await {
             Ok(token_account) => {
                 if let Some(token_account) = token_account {
-                    println!("{:} ORZ", token_account.token_amount.ui_amount_string);
+                    println!("{:} ORE", token_account.token_amount.ui_amount_string);
                 } else {
                     println!("Account not found");
                 }
